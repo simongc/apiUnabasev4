6ba066bcfa0e0e9c2fc8c1dc4bb2006779016611
Simon Gomes <xtgomes@gmail.com>
2018-09-12 17:01:24 -0300

    refactor name of index routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,13 @@ const routes = require('express').Router();
 
 
 
-const auth = require('./auth');
+const auth = require('./auth/auth');
 routes.use('/auth', auth);
 
-const users = require('./users');
+const users = require('./users/users');
 routes.use('/users', users);
 
-const business = require('./business');
+const business = require('./business/business');
 routes.use('/business', business);
 
 
